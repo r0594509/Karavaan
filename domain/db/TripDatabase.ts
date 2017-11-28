cc55d7bb993cef01fccf6828b71436e710f5ba19
Nathan Olmanst <nat@olmanst.net>
2017-11-28 12:08:47 +0100

    db class

---
+++ domain/db/TripDatabase.ts
@@ -0,0 +1,34 @@
+import { Trip } from "../model/Trip";
+import { Person } from "../model/Person";
+import { Expense } from "../model/Expense";
+
+class TripDatabase {
+
+    trips: Array<Trip>;
+
+
+    getTrips() {
+        return this.trips;
+    }
+
+    getTrip(id: Number) {
+        this.trips.forEach(element => {
+            if (element.id == id)
+                return element;
+        });
+    }
+
+    getTripExpensesForPerson(trip: Trip ,person: Person) {
+        var tmp = Array<Expense>();        
+        trip.expenses.forEach(element => {
+            element.persons.forEach(element2 => {
+                if (element2 == person)
+                    tmp.push(element);
+               });
+        });
+    }
+
+    addTrip(trip: Trip) {
+        this.trips.push(trip);
+    }
+}
