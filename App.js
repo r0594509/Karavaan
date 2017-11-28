b0418ca451332aaabe8b5ac50c5a6a4699a5ef7d
Dries Hugaerts <dries.hugaerts@student.ucll.be>
2017-11-28 12:08:18 +0100

    adding View

--- App.js
+++ App.js
@@ -1,23 +1,81 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Button, TouchableHighlight, Modal } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import {TabNavigator} from 'react-navigation'
 
-export default class App extends React.Component {
+
+export class ScreenA extends React.Component {
+  static navigationOptions = {
+    tabBarLabel: 'Uitgaven'
+  };
+
+  state = {
+    modalVisible: false,
+  }
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
+
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{flex: 1}}>
+        <View style={{flex: 1}}>
+          <Text>Somthing</Text>
+          <Button
+            title='Uitgave Toevoegen'
+            onPress={() => this.setModalVisible(true)}
+            />
+          <Modal
+            animationType="slide"
+            transparent={false}
+            visible={this.state.modalVisible}
+            onRequestClose={() => {alert("Modal has been closed.")}}
+          >
+          <View style={{marginTop: 22}}>
+          <View>
+            <Text>Hello World!</Text>
+
+            <Button 
+              title='save'
+              onPress={() => {
+              this.setModalVisible(!this.state.modalVisible)
+            }}>
+              <Text>Hide Modal</Text>
+            </Button>
+
+          </View>
+         </View>
+          </Modal>
+        </View>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+export class ScreenB extends React.Component {
+  static navigationOptions = {
+    tabBarLabel: 'Personen',
+  };
+
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <Text>Contents of screen B</Text>
+        <Button
+            title="Personen Toevoegen"
+            onPress={() => this.onPress()}
+            />
+      </View>
+    );
+  }
+}
+
+
+export default App = TabNavigator({
+  TabA: {screen: ScreenA},
+  TabB: {screen: ScreenB},
+}, {
+  tabBarPosition: 'top'
 });
