fb457b5ef7f3685dc5b9db4e3cd07b370fd5acee
Dries Hugaerts <dries.hugaerts@student.ucll.be>
2017-12-05 12:38:49 +0100

    Creating Cards

--- pages/Trips.js
+++ pages/Trips.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button, TouchableHighlight, Modal } from 'react-native';
-import {TabNavigator} from 'react-navigation'
+import { StyleSheet, Text, View, ScrollView, Button, TouchableHighlight, Modal } from 'react-native';
+import { TabNavigator } from 'react-navigation'
 
 
 export class Trips extends React.Component {
@@ -13,39 +13,55 @@ export class Trips extends React.Component {
   }
 
   setModalVisible(visible) {
-    this.setState({modalVisible: visible});
+    this.setState({ modalVisible: visible });
   }
 
   render() {
     return (
-      <View style={{flex: 1}}>
-        <View style={{flex: 1}}>
-          <Text>Somthing</Text>
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 1 }}>
+
+          <ScrollView contentContainer={{ paddingVertical: 20 }}>
+            <TouchableHighlight onPress= {() => alert("clicked")}>
+              <View style={{ flexDirection: "row", height: 100, padding: 20, backgroundColor: "lightgrey"}}>
+                <Text>Somthing</Text>
+              </View>
+            </TouchableHighlight>
+          </ScrollView>
+
+          <View>
           <Button
             title='Uitgave Toevoegen'
+            color = '#850886'
             onPress={() => this.setModalVisible(true)}
-            />
+          />
+          </View>
+
+
           <Modal
             animationType="slide"
             transparent={false}
             visible={this.state.modalVisible}
-            onRequestClose={() => {alert("Modal has been closed.")}}
+            onRequestClose={() => { alert("Modal has been closed.") }}
           >
-          <View style={{marginTop: 22}}>
-          <View>
-            <Text>Hello World!</Text>
+            <View style={{ marginTop: 22 }}>
+              <View>
+                <Text>Hello World!</Text>
 
-            <Button 
-              title='save'
-              onPress={() => {
-              this.setModalVisible(!this.state.modalVisible)
-            }}>
-              <Text>Hide Modal</Text>
-            </Button>
+                <Button
+                  title='save'
+                  onPress={() => {
+                    this.setModalVisible(!this.state.modalVisible)
+                  }}>
+                  <Text>Hide Modal</Text>
+                </Button>
 
-          </View>
-         </View>
+              </View>
+            </View>
           </Modal>
+
+
+
         </View>
       </View>
     );
