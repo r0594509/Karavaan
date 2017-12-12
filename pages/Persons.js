e59d7b5418fc6e45e6472333233da00efce08a23
Nathan Olmanst <nat@olmanst.net>
2017-12-12 10:55:00 +0100

    domain

--- pages/Persons.js
+++ pages/Persons.js
@@ -1,22 +1,25 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Platform, StatusBar } from 'react-native';
 import { StyleSheet, Text, View, Button, TouchableHighlight, Modal } from 'react-native';
-import {TabNavigator} from 'react-navigation'
+import { TabNavigator } from 'react-navigation'
 
 export class Persons extends React.Component {
-    static navigationOptions = {
-      tabBarLabel: 'Personen',
-      tabBarIcon: () => <Image source={'./img/people_white.png'}/>,
-    };
-  
-    render() {
-      return (
-        <View style={{flex: 1}}>
-          <Text>Contents of screen B</Text>
-          <Button
-              title="Personen Toevoegen"
-              onPress={() => this.onPress()}
-              />
-        </View>
-      );
-    }
+  static navigationOptions = {
+    tabBarLabel: 'Personen',
+    tabBarIcon: () => <Image source={'./img/people_white.png'} />,
+  };
+
+  render() {
+    return (
+      <View style={{paddingTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight}}>
+        <Text>Contents of screen B</Text>
+        <Button
+          title="Personen Toevoegen"
+          onPress={() => this.onPress()}
+        />
+      </View>
+    );
   }
+}
+
+//style={{paddingTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight}}
