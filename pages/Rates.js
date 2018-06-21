4cd3a7a0d3f7dbb4f1e5cd038870ae441f48ea5c
r0594509 <nat@olmanst.net>
2018-06-21 02:54:57 +0200

    WIP: Rates page, no logic

--- pages/Rates.js
+++ pages/Rates.js
@@ -9,6 +9,10 @@ import c from '../domain/controller/Controller';
 
 export class Rates extends React.Component {
 
+    state = {
+        currency: null,
+    }
+
     constructor(props) {
         super(props);
     }
@@ -20,7 +24,7 @@ export class Rates extends React.Component {
         let isInList = false;
 
         c.getTrip(this.props.navigation.state.params.id).relevantCurrencies.forEach(element => {
-            currencyOptions.push({value: element.code})
+            editCurrencyDropdown.push({value: element.code})
             if (element.code == c.getTrip(this.props.navigation.state.params.id).defaultCurrency.code) {
                 isInList = true;
             }
@@ -28,24 +32,71 @@ export class Rates extends React.Component {
 
         if (!isInList) {
             let m = c.getTrip(this.props.navigation.state.params.id).defaultCurrency;
-            currencyOptions.push({value: m.code});
+            editCurrencyDropdown.push({value: m.code});
+        }
+
+
+        if (this.state.currency) {
+
+            c.getTrip(this.props.navigation.state.params.id).relevantCurrencies.forEach( element => {
+
+                if ( element.code != this.state.currency) {
+                    
+                    currencyOptions.push(
+                        <View style={styles.FormViewExpensePerson} key={'currency' + element.code}>
+                            <View style={{ flex: 1 }}>
+                                <Text style={styles.FormText}> {this.state.currency} </Text>
+                            </View>
+                            
+                            <View style={{ width: 150 }}>
+                                <TextInput
+                                    style={[styles.FormInput, { marginTop: -2 }]}
+                                    placeholder="rate"
+                                    onChangeText={(x) => console.log(x)}
+                                    keyboardType='numeric'
+                                    //editable='True'
+                                    defaultValue="test value"
+                                ></TextInput>
+                            </View>
+                            
+                            <View style={{ flex: 1 }}>
+                                <Text style={styles.FormText}> {element.code} </Text>
+                            </View>
+                        </View>
+                    );
+                }
+            });
+
+            currencyOptions.push(
+                <TouchableHighlight key='002' onPress={() => console.log("XXX")} style={styles.ButtonLayoutMain}>
+                    <View>
+                        <Text style={styles.ButtonText}>SAVE</Text>
+                    </View>
+                </TouchableHighlight>
+            );
         }
 
         return (
             <View style={styles.mainViewLayout}>
-                <Text>TEST</Text>
 
                 <Dropdown
                     key='001'
                     label='Select currency to edit:'
                     value='None'
-                    data={currencyOptions}
-                    onChangeText={(x) => console.log(x)}
+                    data={editCurrencyDropdown}
+                    onChangeText={(x) => {
+                            this.setState({currency: x});
+                            //this.forceUpdate();
+                        }
+                    }
                     fontSize={20}
                     containerStyle={{ paddingLeft: 20, paddingRight: 20 }}
                     baseColor='rgba(0, 0, 0, 1)'
                     dropdownPosition={1}
                 />
+
+                {currencyOptions}
+
             </View>
         );
     }
